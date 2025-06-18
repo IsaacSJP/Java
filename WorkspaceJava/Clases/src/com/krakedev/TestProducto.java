8efb189d12be1d7102da5d469c759d86badf0e0b
IsaacSJP <isaacjp995@gmail.com>
2025-06-17 19:11:45 -0500

    creacion de las clases Producto y TestProducto

---
+++ WorkspaceJava/Clases/src/com/krakedev/TestProducto.java
@@ -0,0 +1,44 @@
+package com.krakedev;
+
+public class TestProducto {
+
+	public static void main(String[] args) {
+		Producto productoA;
+		productoA=new Producto();
+		Producto productoB;
+		productoB=new Producto();
+		Producto productoC;
+		productoC=new Producto();
+		
+		productoA.nombre="Paracetamol";
+		productoA.descripcion="Analgesico";
+		productoA.precio=1.50;
+		productoA.stockActual=120;
+
+		productoB.nombre="Ibuprofeno";
+		productoB.descripcion="Antiinflamatorio";
+		productoB.precio=2.00;
+		productoB.stockActual=80;
+		
+		productoC.nombre="Omeprazol";
+		productoC.descripcion="Protector gástrico";
+		productoC.precio=3.00;
+		productoC.stockActual=60;
+		
+		System.out.println("productoA.nombre: "+productoA.nombre);
+		System.out.println("productoA.descripcion: "+productoA.descripcion);
+		System.out.println("productoA.precio: "+productoA.precio);
+		System.out.println("productoA.stockActual: "+productoA.stockActual);
+		System.out.println("*******************");
+		System.out.println("productoB.nombre: "+productoB.nombre);
+		System.out.println("productoB.descripcion: "+productoB.descripcion);
+		System.out.println("productoB.precio: "+productoB.precio);
+		System.out.println("productoB.stockActual: "+productoB.stockActual);
+		System.out.println("*******************");
+		System.out.println("productoC.nombre: "+productoC.nombre);
+		System.out.println("productoC.descripcion: "+productoC.descripcion);
+		System.out.println("productoC.precio: "+productoC.precio);
+		System.out.println("productoC.stockActual: "+productoC.stockActual);
+	}
+
+}

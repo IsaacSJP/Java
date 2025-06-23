cdeb49e35816868a7b6d9bf554806787808cc4d2
IsaacSJP <isaacjp995@gmail.com>
2025-06-23 10:26:38 -0500

    aprendisaje del encapsulamiento

---
+++ WorkspaceJava/Clases/src/com/krakedev/TestFecha.java
@@ -0,0 +1,13 @@
+package com.krakedev;
+
+public class TestFecha {
+
+	public static void main(String[] args) {
+		Fecha fecha1=new Fecha();
+		fecha1.setAnio(2);
+		fecha1.setMes(20);
+		fecha1.setDia(5);
+		System.out.println(fecha1.getMes());
+	}
+
+}
